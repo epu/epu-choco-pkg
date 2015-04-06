c83c4c864a87c366751ea2d7fa7f9e8d78cff08d
Erik Michael Purins <erik.purins@imgtec.com>
2015-04-06 15:58:55 -0700

    Add comment about uninstall.html suppression.

--- advanced-installer/tools/chocolateyuninstall.ps1
+++ advanced-installer/tools/chocolateyuninstall.ps1
@@ -1,4 +1,7 @@
-﻿$productId = '{045A6530-6BCD-49DD-BE52-4DB77958C825}'
+﻿# Product spawns browser URL to uninstall.html, unless UPGRADINGPRODUCTCODE is set.
+# Not sure if it's worth setting this in the properties to suppress the browser launch.
+# Might have other unintended consequences. -epu
+$productId = '{045A6530-6BCD-49DD-BE52-4DB77958C825}'
 $silentArgs = '/qn /norestart REMOVE=ALL REBOOT=ReallySuppress'
 $validExitCodes = @(0)
 try {
