9f73ffbd2765226189004ddbf010c499bc3bb41c
Erik Michael Purins <erik.purins@imgtec.com>
2015-04-21 13:49:26 -0700

    Add coreutils portable, and differentiate titles.

---
+++ gnuwin32-coreutils.portable/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿# Ugh. Requires a dep zip also. coreutils-5.3.0-dep.zip
+$ErrorActionPreference = 'Stop';
+$packageName = 'gnuwin32-coreutils.portable'
+$url = 'http://sourceforge.net/projects/gnuwin32/files/coreutils/5.3.0/coreutils-5.3.0-bin.zip'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$checksum = 'aa7ce7f1f2befb930fb156bddea41bc4'
+$checksumType = 'md5'
+Install-ChocolateyZipPackage "$packageName" "$url" "$toolsDir" -checksum $checksum -checksumType $checksumType
+
+#$binRoot = Get-BinRoot
+#$gitInstallDir = Join-Path $binRoot 'git'
+#$deprecatedInstallDir = Join-Path $env:systemdrive 'git'
+#$files = get-childitem $installDir -include *.exe -recurse
+#foreach ($file in $files) {
+#  if (!($file.Name.Contains("git.exe")) -and !($file.Name.Contains("ssh"))) {
+#    #generate an ignore file
+#    New-Item "$file.ignore" -type file -force | Out-Null
+#  }
+#}
+#if (Test-Path $deprecatedInstallDir) { Remove-Item $deprecatedInstallDir -recurse -force -ErrorAction SilentlyContinue }
+#if (Test-Path $gitInstallDir) { Remove-Item $gitInstallDir -recurse -force -ErrorAction SilentlyContinue }
---
+++ gnuwin32-coreutils.portable/tools/chocolateyuninstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop';
+$packageName = 'gnuwin32-coreutils.install'
+$registryUninstallerKeyName = 'CoreUtils-5.3.0_is1' # Is the suffix _is1 standard?
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /NORESTART'
+$validExitCodes = @(0)
+$osBitness = Get-ProcessorBits
+if ($osBitness -eq 64) {
+  $file = (Get-ItemProperty -Path "hklm:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\$registryUninstallerKeyName").UninstallString
+} else {
+  $file = (Get-ItemProperty -Path "hklm:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\$registryUninstallerKeyName").UninstallString
+} 
+Uninstall-ChocolateyPackage -PackageName $packageName -FileType $installerType -SilentArgs $silentArgs -validExitCodes $validExitCodes -File $file
