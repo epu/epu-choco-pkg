5c08d0cd119a2331c4952e27b17ff617b1c29300
Erik Michael Purins <erik.purins@imgtec.com>
2015-04-21 22:25:10 -0700

    Workaround zip sha1 bug, use md5.

--- premake.portable/tools/chocolateyinstall.ps1
+++ premake.portable/tools/chocolateyinstall.ps1
@@ -1,6 +1,8 @@
 ﻿$packageName = 'premake.portable'
 $url = 'http://sourceforge.net/projects/premake/files/Premake/4.3/premake-4.3-windows.zip'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$checksum = 'd1bb552ea3cbd68a3ba8664bc6e43404e6a34de4'
-$checksumType = 'sha1'
+#$checksum = 'd1bb552ea3cbd68a3ba8664bc6e43404e6a34de4'
+#$checksumType = 'sha1'
+$checksum = '0d96e2e039a2d891f7558f16b13de3b1'
+$checksumType = 'md5'
 Install-ChocolateyZipPackage "$packageName" "$url" "$toolsDir" -checksum $checksum -checksumType $checksumType
