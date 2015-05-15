4f0da70c9cc469e74047e329b3337b4f643e0f9a
Erik Michael Purins <erik.purins@imgtec.com>
2015-05-15 15:29:36 -0700

    add gnu make 3.81 portable

---
+++ gnuwin32-make.portable/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿# Ugh. Requires a dep zip also. make-3.81-dep.zip
+$ErrorActionPreference = 'Stop';
+$packageName = 'gnuwin32-make.portable'
+$url = 'http://sourceforge.net/projects/gnuwin32/files/make/3.81/make-3.81-bin.zip'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$checksum = '3521948bc27a31d1ade0dcb23be16d49'
+$checksumType = 'md5'
+Install-ChocolateyZipPackage "$packageName" "$url" "$toolsDir" -checksum $checksum -checksumType $checksumType
+
+# If we make the lib iconv deps another choco package, they'll never install into this package. :/
+# I can either shove all the gnuwin32 into a shared directory and self-manage the individual file removals,
+# I can just also extract and download the deps zip here,
+# or I can try to manually shim things explicitly setting PATH for shims?
+
+$url = 'http://sourceforge.net/projects/gnuwin32/files/make/3.81/make-3.81-dep.zip'
+$checksum = '2edb5b27ab0818b727fb43494dc40a5f'
+Install-ChocolateyZipPackage "$packageName" "$url" "$toolsDir" -checksum $checksum -checksumType $checksumType
---
+++ gnuwin32-make.portable/tools/chocolateyuninstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'gnuwin32-make.portable'
+$zipfilename = $packageName + 'Install.zip'
+# Does this remove shims?
+# Remove-BinFile	
+# This doesn't attrib -r files before trying to delete them.
+# su.exe had a problem on my test system.
+Uninstall-ChocolateyZipPackage -PackageName $packageName -ZipFileName $zipfilename
