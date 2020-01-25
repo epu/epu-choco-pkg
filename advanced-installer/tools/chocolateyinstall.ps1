6311f15da1c277f89c9261ece5499ed21f7cbf4c
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2020-01-25 15:37:25 -0600

    Update to 16.7

--- advanced-installer/tools/chocolateyinstall.ps1
+++ advanced-installer/tools/chocolateyinstall.ps1
@@ -1,13 +1,16 @@
 ﻿$ErrorActionPreference = 'Stop';
-$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url                   = 'https://www.advancedinstaller.com/downloads/16.7/advinst.msi'
+$checksum              = 'b4e5c5b23d2c0aea0d0a4b64e5b029ce4fc9125f48f278b481a481b1313f9733'
 
 $packageArgs = @{
 	packageName    = 'advanced-installer'
 	installerType  = 'MSI'
-	url            = 'https://www.advancedinstaller.com/downloads/16.6.1/advinst.msi'
-	silentArgs     = '/qn /norestart'
+	url            = $url
+	silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
 	validExitCodes = @(0)
-	checksum       = '4e6be0fc03949c53f28c343be9665abd46986e33eb0262b7c3cb6e1c16ddb7af'
+	checksum       = $checksum
 	checksumType   = 'sha256'
 }
+
 Install-ChocolateyPackage @packageArgs
